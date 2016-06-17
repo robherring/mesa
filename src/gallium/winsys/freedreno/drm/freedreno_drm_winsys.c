0e6cacb6d4a3a0d100d5b1f811b4195e14224d45
Rob Herring <robh@kernel.org>
2016-06-17 08:43:08 -0500

    freedreno: use common pipe_screen ref counting
    Use the common pipe_screen ref counting and fd hashing functions.
    The mutex can be dropped as the pipe loader serializes the
    create_screen() and destroy() calls.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Cc: Rob Clark <robclark@freedesktop.org>

--- src/gallium/winsys/freedreno/drm/freedreno_drm_winsys.c
+++ src/gallium/winsys/freedreno/drm/freedreno_drm_winsys.c
@@ -26,102 +26,22 @@
  *    Rob Clark <robclark@freedesktop.org>
  */
 
-#include <sys/stat.h>
-
-#include "pipe/p_context.h"
-#include "pipe/p_state.h"
-#include "util/u_format.h"
-#include "util/u_memory.h"
-#include "util/u_inlines.h"
-#include "util/u_hash_table.h"
-#include "os/os_thread.h"
+#include "util/u_screen.h"
 
 #include "freedreno_drm_public.h"
 
 #include "freedreno/freedreno_screen.h"
 
-static struct util_hash_table *fd_tab = NULL;
-
-static mtx_t fd_screen_mutex = _MTX_INITIALIZER_NP;
-
-static void
-fd_drm_screen_destroy(struct pipe_screen *pscreen)
-{
-	struct fd_screen *screen = fd_screen(pscreen);
-	boolean destroy;
-
-	mtx_lock(&fd_screen_mutex);
-	destroy = --screen->refcnt == 0;
-	if (destroy) {
-		int fd = fd_device_fd(screen->dev);
-		util_hash_table_remove(fd_tab, intptr_to_pointer(fd));
-	}
-	mtx_unlock(&fd_screen_mutex);
-
-	if (destroy) {
-		pscreen->destroy = screen->winsys_priv;
-		pscreen->destroy(pscreen);
-	}
-}
-
-static unsigned hash_fd(void *key)
-{
-	int fd = pointer_to_intptr(key);
-	struct stat stat;
-	fstat(fd, &stat);
-
-	return stat.st_dev ^ stat.st_ino ^ stat.st_rdev;
-}
-
-static int compare_fd(void *key1, void *key2)
-{
-	int fd1 = pointer_to_intptr(key1);
-	int fd2 = pointer_to_intptr(key2);
-	struct stat stat1, stat2;
-	fstat(fd1, &stat1);
-	fstat(fd2, &stat2);
-
-	return stat1.st_dev != stat2.st_dev ||
-			stat1.st_ino != stat2.st_ino ||
-			stat1.st_rdev != stat2.st_rdev;
-}
-
 struct pipe_screen *
 fd_drm_screen_create(int fd)
 {
-	struct pipe_screen *pscreen = NULL;
+	int dupfd;
+	struct pipe_screen *pscreen = pipe_screen_reference(fd);
+	if (pscreen)
+		return pscreen;
 
-	mtx_lock(&fd_screen_mutex);
-	if (!fd_tab) {
-		fd_tab = util_hash_table_create(hash_fd, compare_fd);
-		if (!fd_tab)
-			goto unlock;
-	}
-
-	pscreen = util_hash_table_get(fd_tab, intptr_to_pointer(fd));
-	if (pscreen) {
-		fd_screen(pscreen)->refcnt++;
-	} else {
-		struct fd_device *dev = fd_device_new_dup(fd);
-		if (!dev)
-			goto unlock;
-
-		pscreen = fd_screen_create(dev);
-		if (pscreen) {
-			int fd = fd_device_fd(dev);
-
-			util_hash_table_set(fd_tab, intptr_to_pointer(fd), pscreen);
-
-			/* Bit of a hack, to avoid circular linkage dependency,
-			 * ie. pipe driver having to call in to winsys, we
-			 * override the pipe drivers screen->destroy():
-			 */
-			fd_screen(pscreen)->winsys_priv = pscreen->destroy;
-			pscreen->destroy = fd_drm_screen_destroy;
-		}
-	}
-
-unlock:
-	mtx_unlock(&fd_screen_mutex);
+	dupfd = dup(fd);
+	pscreen = fd_screen_create(fd_device_new(dupfd));
+	pipe_screen_reference_init(pscreen, dupfd);
 	return pscreen;
 }

1ec011172539590351a1d4fb8038e18512412d1a
Rob Herring <robh@kernel.org>
2017-05-23 13:54:19 -0500

    gallium/os: use mmap64 for Android
    Simplify the handling of mmap for Android by using mmap64 instead. mmap64
    may have not existed for Android when this was written, but it's been
    around since 2013.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- src/gallium/auxiliary/os/os_mman.h
+++ src/gallium/auxiliary/os/os_mman.h
@@ -45,10 +45,6 @@
 #  error Unsupported OS
 #endif
 
-#if defined(PIPE_OS_ANDROID)
-#  include <errno.h> /* for EINVAL */
-#endif
-
 #ifdef  __cplusplus
 extern "C" {
 #endif
@@ -56,19 +52,8 @@ extern "C" {
 
 #if defined(PIPE_OS_ANDROID) && !defined(__LP64__)
 
-extern void *__mmap2(void *, size_t, int, int, int, size_t);
-
-static inline void *os_mmap(void *addr, size_t length, int prot, int flags,
-                            int fd, loff_t offset)
-{
-   /* offset must be aligned to 4096 (not necessarily the page size) */
-   if (unlikely(offset & 4095)) {
-      errno = EINVAL;
-      return MAP_FAILED;
-   }
-
-   return __mmap2(addr, length, prot, flags, fd, (size_t) (offset >> 12));
-}
+#  define os_mmap(addr, length, prot, flags, fd, offset) \
+             mmap64(addr, length, prot, flags, fd, offset)
 
 #  define os_munmap(addr, length) \
              munmap(addr, length)
